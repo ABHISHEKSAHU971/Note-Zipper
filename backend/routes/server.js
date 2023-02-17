3bcf11c060c3f8b9c02ae80de9cd9e578f08edf4
ABHISHEKSAHU971 <abhii97136@gmail.com>
2023-02-17 17:09:55 +0530

    changes_server

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -31,9 +31,14 @@ app.use("/notes", noteRouter);
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "./frontend/build/index.html"),
+      function (err) {
+        res.status(500).send(err);
+      }
+    );
   });
 } else {
   app.get("/", (req, res) => {
